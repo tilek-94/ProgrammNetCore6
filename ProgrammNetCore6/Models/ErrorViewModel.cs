e19cfc23d916daafd27f4eb47c2caab6a709c309
tilek-94 <tilek.9420@gmail.com>
2022-09-30 16:11:26 +0600

    first commit

---
+++ ProgrammNetCore6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgrammNetCore6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
